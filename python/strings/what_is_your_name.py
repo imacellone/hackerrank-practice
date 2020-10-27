0827e025a6534d5b5ba302173e02aa2231079b6a
Italo  Macellone <italo.macellone.ext@pentasys.de>
2020-10-27 21:55:56 +0100

    added python/strings/what_is_your_name

---
+++ python/strings/what_is_your_name.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+# Score: 10/10
+
+
+def print_full_name(a, b):
+    print(f"Hello {a} {b}! You just delved into python.")
+
+
+print_full_name(input("First name: "), input("Last name: "))
+
+
+
+
